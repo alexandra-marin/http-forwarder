04000f864aff519a831c799490a0e04fb105247c
Alexandra Marin <alexandra.marin01@gmail.com>
2020-08-20 11:21:34 +0300

    test for success

--- test/api.test.js
+++ test/api.test.js
@@ -14,13 +14,58 @@ describe('GET /api/v1', () => {
 });
 
 describe('POST /api/v1', () => {
-  it('responds with a message', async () => {
+  it('responds with a bad request if events not sent', async () => {
     const response = await request(app)
       .post('/api/v1/')
-      .send({ name: 'john' })
       .set('Accept', 'application/json');
 
     response.status.should.eq(400);
-    response.body.errors[0].msg.should.be.eq('Events cannot be blank');
+    return response.body.errors[0].msg.should.be.eq('Events cannot be blank');
   });
+
+  it('responds with message if successful', async () => {
+    const payload = {
+      events: [
+        {
+          t: 'lift-off',
+          engines: 4,
+          fuel: 78,
+          successful: true,
+          temperature: {
+            engine: 80,
+            cabin: 31
+          },
+          timestamp: 1595244264059,
+          'lat-lon': [
+            -16.270183,
+            168.110748
+          ]
+        },
+        {
+          t: 'landing',
+          engines: 1,
+          fuel: 26,
+          successful: true,
+          temperature: {
+            engine: 80,
+            cabin: 31
+          },
+          timestamp: 1595524813145,
+          'lat-lon': [
+            51.769455,
+            182.818610
+          ],
+        }
+      ]
+    };
+    const expected = 'Thanks for contacting Spaceship\nThanks for contacting Spaceship\n\nhello from M0nit0r\nhello from M0nit0r\n\nthis is skyanalytics\nthis is skyanalytics\n';
+
+    const response = await request(app)
+      .post('/api/v1/')
+      .set('Accept', 'application/json')
+      .send(payload);
+
+    response.status.should.be.eq(200);
+    response.body.message.should.be.eq(expected);
+  }).timeout('5s');
 });
